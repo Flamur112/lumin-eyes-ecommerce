64811d717a243ab5ae31a6b9538bb305b16a3511
Flamur <34803365+Flamur112@users.noreply.github.com>
2024-01-01 11:05:35 -1100

    Initial commit for the admin-panel/dashboard branch

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->group(function(){
+
+    Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
+
+});
+
