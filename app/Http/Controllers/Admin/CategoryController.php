c3b300b94b3f7a14bebe1f9c9ebee4fda3b0877c
Flamur <34803365+Flamur112@users.noreply.github.com>
2024-01-24 11:13:34 -1100

    Categories

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -23,6 +23,8 @@ class CategoryController extends Controller
     public function store(CategoryFormRequest  $request)
     {
 
+        dd('request');
+
         $validatedData = $request->validated();
 
         $category = new Category;
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -2,23 +2,8 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-md-12">
-
-        @if (session('message'))
-        <div class="alert alert-success">{{ session('message')}}</div>
-        @endif
-
-        <div class="card">
-            <div class="card-header">
-                <h3>Category
-                    <a href="{{ url('admin/category/create') }}" class="btn btn-primary btn-sm float-end">Add Category</a>
-                </h3>
-            </div>
-            <div class="card-body">
-            </div>
-        </div>
-    </div>
+<div>
+    <livewire:admin.category.index />
 </div>
 
 @endsection
